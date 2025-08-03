22b7321ac4ae416066c92ce5d0736f6c52c87b88
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2025-08-03 14:26:28 +1000

    feat: complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
+// Time complexity O(n) space complexity O(n)
+
+fun maxSum(nums: IntArray): Int {
+    val max = nums.max()
+    if (max < 0) {
+        return max
+    }
+
+    val uniqueNums = nums.toSet().filter { it > 0 }
+
+    return uniqueNums.sum()
+}
+
 fun main() {
-    println("Hello world")
+    val input: IntArray = intArrayOf(1, 2, -1, -2, 1, 0, -1)
+    val result = maxSum(input)
+    println(result)
 }
